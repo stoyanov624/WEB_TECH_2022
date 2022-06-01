e80724a7421fd271ca636216b555a84af312fc66
Ivo <paradoxa624@gmail.com>
2022-06-02 00:17:47 +0300

    added cursor based pagination

--- pagination/index.js
+++ pagination/index.js
@@ -20,7 +20,7 @@ app.get("/all_stocks", async(req, res) => {
 })
 
 // get paginated stocks
-app.get("/stocks", async(req, res) => {
+app.get("/stocks-with-offset", async(req, res) => {
     try {
         const page = req.query?.page || DEFAULT_PAGE;
         const size = req.query?.size || DEFAULT_NUMBER_OF_ELEMENTS;
@@ -33,6 +33,36 @@ app.get("/stocks", async(req, res) => {
         const offset = (parseInt(page) - 1) * size; 
         const stocks = await pool.query(`SELECT * FROM stocks LIMIT ${limit} OFFSET ${offset}`);
         res.json(stocks.rows);
+    } catch(error) {
+        res.sendStatus(500).send(error.message);
+    }
+});
+
+app.get("/stocks-cursor-based", async(req, res) => {
+    try {
+        let previousCursor = req.query?.prev || null;
+        let nextCursor = req.query?.next || null;
+        const size = req.query?.size || DEFAULT_NUMBER_OF_ELEMENTS;
+        const limit = parseInt(size);
+        let query = '';
+
+        if(!previousCursor && !nextCursor) {
+            query = `SELECT * FROM STOCKS LIMIT ${limit}`;
+        }
+
+        if(nextCursor !== null) {
+            query = `SELECT * FROM STOCKS WHERE id > ${nextCursor} LIMIT ${limit}`;
+        }
+
+        if(previousCursor !== null) {
+            query = `SELECT * FROM STOCKS WHERE id < ${nextCursor} LIMIT ${limit}`;
+        }
+        
+        const data = (await pool.query(query)).rows;
+        previousCursor = data[0].id;
+        nextCursor = data[data.length - 1].id;
+        const stocks = {data: data, previousCursor: previousCursor, nextCursor: nextCursor};
+        res.json(stocks);
     } catch(error) {
         res.sendStatus(500).send(error.message);
     }
